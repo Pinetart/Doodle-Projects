92522a716182fc4f5e35136331e689f053f67284
Zane Birkett <Zane.birkett@houstydesigns.com>
2022-03-04 15:28:30 -0400

    Fixed Logo Link

--- src/Components/UI/Navbar.js
+++ src/Components/UI/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <Link to="./">
+      <Link to="/">
         <h1>React Project II</h1>
       </Link>
       <div className="links">

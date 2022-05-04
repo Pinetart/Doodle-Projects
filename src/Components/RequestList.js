085c5a2a4e9aec4b6bbbdb84312bb7e0186ab278
Zane Birkett <axelbirkett@gmail.com>
2022-05-04 11:16:02 -0400

    Table Template

--- src/Components/RequestList.js
+++ src/Components/RequestList.js
@@ -14,14 +14,28 @@ const Requestlist = ({ requests, updateDashboard, allRequest }) => {
   };
 
   return (
-    <div>
+    <table className="tablelist">
+      <tr>
+        <th>Requestor</th>
+        <th>Title</th>
+        <th>Status</th>
+        <th>Priority</th>
+        <th>Date</th>
+        <th></th>
+      </tr>
       {requests.map((request) => (
-        <div className="request-preview" key={request.id}>
-          <Link to={`/requests/${request.id}`}>
-            <h2>{request.description}</h2>
-            <p>Written by {request.author}</p>
-          </Link>
-          <div className="actions">
+        <tr>
+          <td>
+            <p style={{ fontWeight: "500" }}>{request.requestor}</p>
+            <p style={{ fontWeight: "300", color: "#9B9B9B" }}>
+              {request.requestoremail}
+            </p>
+          </td>
+          <td>{request.title}</td>
+          <td>{request.status}</td>
+          <td>{request.urgency}</td>
+          <td>{request.date}</td>
+          <td>
             <img src={editIcon} alt="edit request" />
             {!isLoading && (
               <img
@@ -41,10 +55,41 @@ const Requestlist = ({ requests, updateDashboard, allRequest }) => {
             {isLoading && (
               <img src={loadingIcon} className="loading" alt="loading" />
             )}
-          </div>
-        </div>
+          </td>
+        </tr>
       ))}
-    </div>
+    </table>
+    // <div>
+    //   {requests.map((request) => (
+    //     <div className="request-preview" key={request.id}>
+    //       <Link to={`/requests/${request.id}`}>
+    //         <h2>{request.description}</h2>
+    //         <p>Written by {request.author}</p>
+    //       </Link>
+    //       <div className="actions">
+    //         <img src={editIcon} alt="edit request" />
+    //         {!isLoading && (
+    //           <img
+    //             src={deleteIcon}
+    //             onClick={(e) => {
+    //               setIsLoading(true);
+    //               fetch(`http://localhost:8000/requests/${request.id}`, {
+    //                 method: "DELETE",
+    //               }).then(() => {
+    //                 handleClick(request.id);
+    //                 setIsLoading(false);
+    //               });
+    //             }}
+    //             alt="add request"
+    //           />
+    //         )}
+    //         {isLoading && (
+    //           <img src={loadingIcon} className="loading" alt="loading" />
+    //         )}
+    //       </div>
+    //     </div>
+    //   ))}
+    // </div>
   );
 };
 
